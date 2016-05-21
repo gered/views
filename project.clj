93617a4857b6f9c81300654d29838545e6d51669
gered <gered@blarg.ca>
2016-05-21 19:27:47 -0400

    update project.clj
    i don't need a bunch of this stuff in this fork. no sense keeping it
    around (can re-add later if need-be)

--- project.clj
+++ project.clj
@@ -1,35 +1,21 @@
-(defproject kirasystems/views "1.4.9-SNAPSHOT"
-  :description "A view to the past helps navigate the future."
+(defproject gered/views "1.5-SNAPSHOT"
+  :description  "A view to the past helps navigate the future."
+  :url          "https://github.com/gered/views"
 
-  :url "https://github.com/kirasystems/views"
-
-  :license {:name "MIT License"
-            :url "http://opensource.org/licenses/MIT"}
-
-  :repositories [["releases" {:url "https://clojars.org/repo"
-                              :sign-releases false
-                              :username :env
-                              :password :env}]]
+  :license      {:name "MIT License"
+                 :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/tools.logging "0.3.1"]
-                 [clj-logging-config "1.9.12"]
                  [prismatic/plumbing "0.5.3"]
                  [pjstadig/humane-test-output "0.8.0"]]
 
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :test {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                                   [org.clojure/data.generators "0.1.2"]]
+  :profiles     {:provided
+                 {:dependencies
+                  [[org.clojure/clojure "1.8.0"]]}
 
-                    :injections [(require 'pjstadig.humane-test-output)
-                                 (pjstadig.humane-test-output/activate!)]}}
-
-  :plugins [[lein-ancient "0.6.10"]
-            [lein-environ "1.0.3"]]
-
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag" "--no-sign"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]
-                  ["vcs" "push"]])
+                 :test
+                 {:dependencies
+                  [[pjstadig/humane-test-output "0.8.0"]]
+                  :injections
+                  [(require 'pjstadig.humane-test-output)
+                   (pjstadig.humane-test-output/activate!)]}})
